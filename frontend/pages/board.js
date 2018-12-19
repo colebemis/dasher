5cc603373734f8c341255bd050652424c6228fa5
Cole Bemis <colebemis@gmail.com>
2018-12-19 04:19:22 -0800

    Add page titles

--- frontend/pages/board.js
+++ frontend/pages/board.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import { withRouter } from 'next/router'
 
 const Board = ({ router }) => {
   return (
     <div>
+      <Head>
+        <title>{router.query.id} | Dasher</title>
+      </Head>
       <h1>Board {router.query.id}</h1>
     </div>
   )
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import Link from 'next/link'
 
 const Index = () => {
   return (
     <div>
+      <Head>
+        <title>Dasher</title>
+      </Head>
       <h1>Hello World</h1>
       <Link href="/board?id=1">
         <a>Board 1</a>
