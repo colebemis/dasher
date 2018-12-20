24548656abaad0f542e3bc81d18204710bb4a7c8
Cole Bemis <colebemis@gmail.com>
2018-12-20 00:16:45 -0800

    Set up Apollo Client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,26 @@
 import App, { Container } from 'next/app'
 import Head from 'next/head'
+import { ApolloProvider } from 'react-apollo'
+import withApollo from '../lib/withApollo'
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props
-
+    const { apollo, Component, pageProps } = this.props
     return (
       <Container>
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <title>Dasher</title>
-        </Head>
-        <Component {...pageProps} />
+        <ApolloProvider client={apollo}>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <title>Dasher</title>
+          </Head>
+          <Component {...pageProps} />
+        </ApolloProvider>
       </Container>
     )
   }
 }
 
-export default MyApp
+export default withApollo(MyApp)
