ac119422896ef104c6bea53d5c8a543811c5c85b
Cole Bemis <colebemis@gmail.com>
2018-12-20 14:26:24 -0800

    Fix next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,25 @@
-const withTypescript = require('@zeit/next-typescript')
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION
+    ? require('next/constants')
+    : require('next-server/constants')
 
-module.exports = withTypescript({
-  publicRuntimeConfig: {
-    apiEndpoint: process.env.API_ENDPOINT || 'http://localhost:4000',
-  },
-})
+module.exports = phase => {
+  const config = {
+    publicRuntimeConfig: {
+      apiEndpoint: process.env.API_ENDPOINT || 'http://localhost:4000',
+    },
+  }
+
+  // Don't use build plugins in production phase
+  // https://git.io/fhvat
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return config
+  }
+
+  const withTypescript = require('@zeit/next-typescript')
+
+  return withTypescript(config)
+}
