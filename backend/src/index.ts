3a06b51bb0455d669bcc6ef6f8bbe862574b4737
Cole Bemis <colebemis@gmail.com>
2018-12-28 16:24:34 -0800

    Set up prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,25 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer, gql, IResolvers } from 'apollo-server'
+import { importSchema } from 'graphql-import'
+import path from 'path'
+import { prisma, Prisma } from './generated/prisma'
 
-const typeDefs = gql`
-  type Query {
-    message: String
-  }
-`
+interface Context {
+  prisma: Prisma
+}
 
-const resolvers = {
+const typeDefs = gql(importSchema(path.resolve(__dirname, 'schema.graphql')))
+
+const resolvers: IResolvers<any, Context> = {
   Query: {
     message: () => 'Hello World',
+    users: (parent, args, context, info) => context.prisma.users(),
   },
 }
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: { prisma },
 })
 
 server.listen().then(({ url }) => console.log(`Server ready at ${url}`))
