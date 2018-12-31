37e50100efb394d8298a6ea6416484821089b778
Cole Bemis <colebemis@gmail.com>
2018-12-30 22:09:01 -0800

    Similify createClient function

--- frontend/lib/withApollo.ts
+++ frontend/lib/withApollo.ts
@@ -1,27 +1,11 @@
 import ApolloClient from 'apollo-boost'
-import withApollo, { InitApolloOptions } from 'next-with-apollo'
+import withApollo from 'next-with-apollo'
 import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
 
-function createClient({ headers }: InitApolloOptions<{}>) {
-  const { apiEndpoint } = publicRuntimeConfig
-
-  // node-fetch (used by apollo-client) requires absolute URLs,
-  // so we have to resolve relative URLs when running on the server
-  // https://git.io/fheC6
-
-  let uri
-
-  // If we are running on the server and apiEndpoint is a relative URL
-  if (headers && apiEndpoint.startsWith('/')) {
-    // Q: Is there a way to avoid hardcoding the protocol?
-    uri = `https://${headers.host}${apiEndpoint}`
-  } else {
-    uri = apiEndpoint
-  }
-
-  return new ApolloClient({ uri })
+function createClient() {
+  return new ApolloClient({ uri: publicRuntimeConfig.apiEndpoint })
 }
 
 export default withApollo(createClient)
