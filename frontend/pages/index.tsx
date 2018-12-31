b553cda58074b3d2394ca2968886ba4ecf164a63
Cole Bemis <colebemis@gmail.com>
2018-12-31 04:20:53 -0800

    Set up graphql codegen on the frontend

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,17 +1,23 @@
 import Link from 'next/link'
-import MessageQuery from '../components/MessageQuery'
+import { GetUsersComponent } from '../__generated__/graphql'
 
 const Index: React.FunctionComponent<{}> = () => {
   return (
     <div css={{ color: 'red' }}>
-      <MessageQuery>
+      <GetUsersComponent>
         {({ loading, error, data }) => {
           if (loading) return <p>Loading...</p>
           if (error) return <p>Error: {error.message}</p>
           if (!data) return <p>Not found</p>
-          return <h1>{data.message}</h1>
+          return (
+            <ul>
+              {data.users.map(user => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+            </ul>
+          )
         }}
-      </MessageQuery>
+      </GetUsersComponent>
       <Link href="/board?id=1">
         <a>Board 1</a>
       </Link>
