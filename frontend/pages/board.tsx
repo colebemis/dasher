2c558127e8d72822115f92d302c34c4b705c1d57
Cole Bemis <colebemis@gmail.com>
2018-12-31 16:04:35 -0800

    Create getBoard query

--- frontend/pages/board.tsx
+++ frontend/pages/board.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { withRouter, WithRouterProps } from 'next/router'
+import { GetBoardComponent } from '../__generated__/graphql'
 
 interface Query {
   id?: string
@@ -8,18 +9,28 @@ interface Query {
 
 const Board: React.FunctionComponent<WithRouterProps<Query>> = ({ router }) => {
   // Q: When will router or router.query be undefined?
-  if (!router || !router.query) return null
+  if (!router || !router.query || !router.query.id) return <p>Not found</p>
 
   return (
-    <div>
-      <Head>
-        <title>{router.query.id} | Dasher</title>
-      </Head>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <h1>Board {router.query.id}</h1>
-    </div>
+    <GetBoardComponent variables={{ id: router.query.id }}>
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>
+        if (error) return <p>Error: {error.message}</p>
+        if (!data || !data.board) return <p>Not found</p>
+        return (
+          <div>
+            <Head>
+              <title>{data.board.name} | Dasher</title>
+            </Head>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+            <h1>{data.board.name}</h1>
+            <code>{data.board.query}</code>
+          </div>
+        )
+      }}
+    </GetBoardComponent>
   )
 }
 
