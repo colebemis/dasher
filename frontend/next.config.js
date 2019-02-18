6f96c80bce12a9b8b977591651aaf431f27586d4
Cole Bemis <colebemis@gmail.com>
2019-02-18 00:38:19 -0800

    Set up callback page

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,8 @@ const withTypescript = require('@zeit/next-typescript')
 const config = {
   env: {
     apiEndpoint: process.env.API_ENDPOINT || 'http://localhost:4000',
+    githubClientId: '6394b4fd5f4f0606b2f7',
+    githubRedirectUri: 'http://localhost:3000/callback',
   },
 }
 
