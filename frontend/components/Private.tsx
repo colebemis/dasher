2ac2756a6fa3a8854308ef50c53969029e00923d
Cole Bemis <colebemis@gmail.com>
2019-02-18 08:50:17 -0800

    Rename Login to SignIn

--- frontend/components/Private.tsx
+++ frontend/components/Private.tsx
@@ -1,7 +1,7 @@
-import Login from './Login'
+import SignIn from './SignIn'
 
 const Private: React.FC<{}> = ({ children }) => {
-  return true ? <Login /> : <>{children}</>
+  return true ? <SignIn /> : <>{children}</>
 }
 
 export default Private
