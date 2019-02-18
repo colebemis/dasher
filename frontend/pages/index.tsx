bf27e51bf8abe20fc47649c2482071e30c6f8b37
Cole Bemis <colebemis@gmail.com>
2019-02-18 01:14:54 -0800

    Add Login and Private components

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,28 +1,31 @@
 import Link from 'next/link'
+import Private from '../components/Private'
 import { GetUsersComponent } from '../__generated__/graphql'
 
 const Index: React.FC<{}> = () => {
   return (
-    <div css={{ color: 'red' }}>
-      <GetUsersComponent>
-        {({ loading, error, data }) => {
-          if (loading) return <p>Loading...</p>
-          if (error) return <p>Error: {error.message}</p>
-          if (!data) return <p>Not found</p>
+    <Private>
+      <div css={{ color: 'red' }}>
+        <GetUsersComponent>
+          {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>
+            if (error) return <p>Error: {error.message}</p>
+            if (!data) return <p>Not found</p>
 
-          return (
-            <ul>
-              {data.users.map(user => (
-                <li key={user.id}>{user.name}</li>
-              ))}
-            </ul>
-          )
-        }}
-      </GetUsersComponent>
-      <Link href="/board?id=1">
-        <a>Board 1</a>
-      </Link>
-    </div>
+            return (
+              <ul>
+                {data.users.map(user => (
+                  <li key={user.id}>{user.name}</li>
+                ))}
+              </ul>
+            )
+          }}
+        </GetUsersComponent>
+        <Link href="/board?id=1">
+          <a>Board 1</a>
+        </Link>
+      </div>
+    </Private>
   )
 }
 
