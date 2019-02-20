10b529cc853d9bfbf5af6555af67cd3e5eb2eb68
Cole Bemis <colebemis@gmail.com>
2019-02-19 23:55:57 -0800

    Update server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { ApolloServer } from 'apollo-server'
+import { GraphQLServer } from 'graphql-yoga'
 import { stringArg } from 'nexus'
 import { makePrismaSchema, prismaObjectType } from 'nexus-prisma'
 import path from 'path'
@@ -8,17 +8,25 @@ import { prisma } from './__generated__/prisma-client'
 const Query = prismaObjectType({
   name: 'Query',
   definition(t) {
-    t.prismaFields(['users', 'board'])
+    t.prismaFields(['board'])
   },
 })
 
 const Mutation = prismaObjectType({
   name: 'Mutation',
   definition(t) {
-    t.field('logIn', {
+    t.field('signIn', {
+      // Q: What should the return type be?
       type: 'Boolean',
       args: { githubCode: stringArg({ required: true }) },
-      resolve: (root, args) => true,
+      resolve: async (root, args, context) => {
+        // fetch github token
+        // fetch github user id
+        // find or create user
+        // generate JWT that encodes the user id and github token
+        // store JWT in an httpOnly cookie
+        return true
+      },
     })
   },
 })
@@ -33,9 +41,21 @@ const schema = makePrismaSchema({
     schema: path.join(__dirname, './__generated__/schema.graphql'),
     typegen: path.join(__dirname, './__generated__/nexus.ts'),
   },
+  typegenAutoConfig: {
+    sources: [
+      {
+        source: path.join(__dirname, './types.ts'),
+        alias: 'types',
+      },
+    ],
+    contextType: 'types.Context',
+  },
 })
 
-const server = new ApolloServer({ schema })
+const server = new GraphQLServer({
+  schema,
+  context: { prisma },
+})
 
 // tslint:disable-next-line:no-console
-server.listen().then(({ url }) => console.log(`Server ready at ${url}`))
+server.start(({ port }) => console.log(`Server running on port ${port}`))
---
+++ backend/src/types.ts
@@ -0,0 +1,5 @@
+import { Prisma } from './__generated__/prisma-client'
+
+export interface Context {
+  prisma: Prisma
+}
