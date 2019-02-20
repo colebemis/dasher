6a7233f33a8562cb2a04b9bf6f4d6c9a05fabfa7
Cole Bemis <colebemis@gmail.com>
2019-02-19 19:44:40 -0800

    Add comments to the callback page

--- frontend/pages/callback.tsx
+++ frontend/pages/callback.tsx
@@ -1,7 +1,10 @@
 import { NextContext, NextFC } from 'next'
-import Router from 'next/router'
 import { AppContext } from 'next-with-apollo'
+import Router from 'next/router'
 import { SignInDocument } from '../__generated__/graphql'
+
+// GitHub redirects back to this page with a temporary `code` in the query string.
+// Reference: https://git.io/fhd1D
 
 const Callback: NextFC<{}, {}, AppContext> = () => {
   return null
@@ -10,10 +13,16 @@ const Callback: NextFC<{}, {}, AppContext> = () => {
 Callback.getInitialProps = async context => {
   if (!context.query.code) redirect(context, '/')
 
-  // Sign in
+  // The API generates a JWT and stores it in an httpOnly cookie that can
+  // be passed between the client and server to authenticate requests.
   await context.apolloClient.mutate({
     mutation: SignInDocument,
-    variables: { githubCode: context.query.code },
+    variables: {
+      githubCode: context.query.code,
+      // The API exchanges this `githubCode` for an access token that it
+      // can use to make requests to GitHub's API on behalf of the user.
+      // Reference: https://git.io/fhd1D
+    },
   })
 
   redirect(context, '/')
