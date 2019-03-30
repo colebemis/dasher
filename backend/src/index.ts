f990923772592d3e22ec0011910d5831d42df624
Cole Bemis <colebemis@gmail.com>
2019-03-30 16:20:00 -0700

    Request and store token

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga'
-import { stringArg } from 'nexus'
+import { stringArg, objectType } from 'nexus'
 import { makePrismaSchema, prismaObjectType } from 'nexus-prisma'
 import path from 'path'
 import datamodelInfo from './__generated__/nexus-prisma'
@@ -16,24 +16,29 @@ const Mutation = prismaObjectType({
   name: 'Mutation',
   definition(t) {
     t.field('signIn', {
-      // Q: What should the return type be?
-      type: 'Boolean',
+      type: SignInPayload,
       args: { githubCode: stringArg({ required: true }) },
       resolve: async (root, args, context) => {
         // fetch github token
         // fetch github user id
         // find or create user
-        // generate JWT that encodes the user id and github token
-        // store JWT in an httpOnly cookie
-        context.response.cookie('foo', 'bar', { httpOnly: true })
-        return true
+        // generate JWT
+        const token = 'fake_token'
+        return { token }
       },
     })
   },
 })
 
+const SignInPayload = objectType({
+  name: 'SignInPayload',
+  definition(t) {
+    t.string('token')
+  },
+})
+
 const schema = makePrismaSchema({
-  types: [Query, Mutation],
+  types: [Query, Mutation, SignInPayload],
   prisma: {
     datamodelInfo,
     client: prisma,
@@ -58,5 +63,15 @@ const server = new GraphQLServer({
   context: params => ({ ...params, prisma }),
 })
 
-// tslint:disable-next-line:no-console
-server.start(({ port }) => console.log(`Server running on port ${port}`))
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: 'http://localhost:3000',
+    },
+  },
+  ({ port }) => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server running on port ${port}`)
+  },
+)
