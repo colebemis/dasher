361e12f54d759fba128b03279caabf472866998a
Cole Bemis <colebemis@gmail.com>
2019-04-11 22:29:12 -0700

    Update default text color

--- frontend/components/GlobalStyles.tsx
+++ frontend/components/GlobalStyles.tsx
@@ -10,7 +10,7 @@ const GlobalStyles: React.FC<{}> = () => {
           margin: 0,
           fontFamily: theme.fonts.sansSerif,
           lineHeight: theme.lineHeights.normal,
-          color: theme.colors.gray[9],
+          color: theme.colors.gray[8],
           backgroundColor: theme.colors.gray[0],
         },
         '*, *::before, *::before': {
