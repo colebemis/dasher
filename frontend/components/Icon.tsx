ef38e55b85ec4ca21a7481f56fd818b725d780d3
Cole Bemis <colebemis@gmail.com>
2019-04-25 20:05:56 -0700

    Add a plus icon

--- frontend/components/Icon.tsx
+++ frontend/components/Icon.tsx
@@ -42,4 +42,12 @@ export const ExternalLinkIcon: React.FC<
   )
 }
 
+export const PlusIcon: React.FC<React.SVGProps<SVGSVGElement>> = props => {
+  return (
+    <Icon {...props}>
+      <path d="M14 8C14 8.55228 13.5523 9 13 9H9V13C9 13.5523 8.55228 14 8 14C7.44772 14 7 13.5523 7 13V9H3C2.44772 9 2 8.55228 2 8C2 7.44772 2.44772 7 3 7H7V3C7 2.44772 7.44772 2 8 2C8.55228 2 9 2.44772 9 3V7H13C13.5523 7 14 7.44772 14 8Z" />
+    </Icon>
+  )
+}
+
 export default Icon
