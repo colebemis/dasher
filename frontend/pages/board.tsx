d347a5241552fa04de4062b9b98a9f1c2b4c0a78
Cole Bemis <colebemis@gmail.com>
2019-04-26 13:29:43 -0700

    Style board header

--- frontend/pages/board.tsx
+++ frontend/pages/board.tsx
@@ -1,9 +1,13 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { withRouter, WithRouterProps } from 'next/router'
 import Private from '../components/Private'
 import { GetBoardComponent } from '../__generated__/graphql'
 import BoardMenu from '../components/BoardMenu'
+import AccountMenu from '../components/AccountMenu'
+import theme from '../theme'
+import { ChevronLeftIcon } from '../components/Icon'
+import Link from '../components/Link'
 
 interface Query {
   id?: string
@@ -13,6 +17,24 @@ const Board: React.FC<WithRouterProps<Query>> = ({ router }) => {
   if (!router || !router.query || !router.query.id) return <p>Not found</p>
   return (
     <Private>
+      <div
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          padding: `${theme.space[4]} ${theme.space[4]} 0`,
+        }}
+      >
+        <NextLink href="/" passHref={true}>
+          <Link css={{ color: theme.colors.gray[7] }}>
+            <ChevronLeftIcon
+              css={{ marginRight: theme.space[1], color: theme.colors.gray[6] }}
+            />
+            Boards
+          </Link>
+        </NextLink>
+        <div css={{ margin: '0 auto' }} />
+        <AccountMenu />
+      </div>
       <GetBoardComponent variables={{ id: router.query.id }}>
         {({ loading, error, data }) => {
           if (loading) return <p>Loading...</p>
@@ -23,16 +45,51 @@ const Board: React.FC<WithRouterProps<Query>> = ({ router }) => {
               <Head>
                 <title>{data.board.name} | Dasher</title>
               </Head>
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-              <h1>{data.board.name}</h1>
-              <p>{data.board.query}</p>
-              <BoardMenu
-                id={data.board.id}
-                name={data.board.name}
-                query={data.board.query}
-              />
+              <div
+                css={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  padding: theme.space[5],
+                  [theme.mediaQueries.medium]: {
+                    padding: theme.space[6],
+                  },
+                }}
+              >
+                <div>
+                  <h1
+                    css={{
+                      display: 'inline-block',
+                      margin: 0,
+                      fontSize: theme.fontSizes[5],
+                      lineHeight: theme.lineHeights.tight,
+                      [theme.mediaQueries.medium]: {
+                        fontSize: theme.fontSizes[6],
+                      },
+                    }}
+                  >
+                    {data.board.name}
+                  </h1>
+                  <BoardMenu
+                    id={data.board.id}
+                    name={data.board.name}
+                    query={data.board.query}
+                    css={{ marginLeft: theme.space[2] }}
+                  />
+                </div>
+                <span
+                  css={{
+                    fontFamily: theme.fonts.monospace,
+                    fontSize: theme.fontSizes[1],
+                    color: theme.colors.gray[7],
+                    [theme.mediaQueries.medium]: {
+                      marginTop: theme.space[1],
+                      fontSize: theme.fontSizes[2],
+                    },
+                  }}
+                >
+                  {data.board.query}
+                </span>
+              </div>
               {data.board.columns
                 ? data.board.columns.map(column => (
                     <div key={column.id}>
