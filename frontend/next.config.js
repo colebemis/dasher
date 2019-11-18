ac4b3f074bc5ead42accf1c357a885d764ff4901
Cole Bemis <colebemis@github.com>
2019-11-17 23:15:40 -0800

    Deploy frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,12 @@
 const withTypescript = require('@zeit/next-typescript')
 
 const config = {
+  target: 'serverless',
   env: {
-    apiEndpoint: process.env.API_ENDPOINT || 'http://localhost:4000',
-    githubClientId: '6394b4fd5f4f0606b2f7',
-    githubRedirectUri: 'http://localhost:3000/callback',
+    apiEndpoint: process.env.API_ENDPOINT || 'http://localhost:4000/graphql',
+    githubClientId: process.env.GITHUB_CLIENT_ID || '6394b4fd5f4f0606b2f7',
+    githubRedirectUri:
+      process.env.GITHUB_REDIRECT_URI || 'http://localhost:3000/callback',
   },
 }
 
